e362a963143c44e5196ee848783ab49c667f08d3
d0monation <domonation@gmx.at>
2022-04-03 10:40:53 +0200

    Wecker3D Sound update

--- Wecker.gd
+++ Wecker.gd
@@ -10,6 +10,8 @@ extends Spatial
 func _ready():
 	pass # Replace with function body.
 
+func _play_alarm_sfx():
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
