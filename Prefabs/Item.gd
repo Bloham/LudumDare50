0a93140cd756446e8d6cacc417c417d16f5d5615
AR <swordmageART@gmail.com>
2022-04-03 16:11:43 +0200

    remoced area from item prefab

--- Prefabs/Item.gd
+++ Prefabs/Item.gd
@@ -4,11 +4,12 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var spielwelt
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	spielwelt = get_tree().get_root().get_node("Spielwelt")
 
 func albtraum():
 #	print(self.name, " ist nun im Alptraumland")
@@ -17,16 +18,9 @@ func albtraum():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var wm = get_tree().get_root().get_node("Spielwelt").get_node("AllWecker")
+	var wm = spielwelt.get_node("AllWecker")
 	for w in wm.get_children():
 		var wa = w.get_node("WeckerAsset").get_node("WeckerArea")
 		var dist = translation.distance_to(wa.global_transform.origin)
 		if(wa.currentRadius > dist):
 			albtraum()
-
-
-func _on_Area_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	pass
-#	if area.get_name() == "WeckerArea":
-#		albtraum()
-		
