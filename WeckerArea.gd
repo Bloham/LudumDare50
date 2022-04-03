a7f93dfaef0478d97b1d800fabc54a897f8d4518
Ben Vehling <silverplay@hotmail.com>
2022-04-03 11:12:07 +0200

    Wecker radiius wächst nach zeit (aber wird nicht von item erkannt?)

--- WeckerArea.gd
+++ WeckerArea.gd
@@ -1,23 +1,29 @@
 extends Area
 
+export var radiusIncrease = 5
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-onready var radius = get_node("CollisionShape")
+onready var collider = get_node("CollisionShape").shape
 onready var timer = get_node("Timer")
+
+var currentRadius = 45
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Der Wecker Radius ist: ",radius.shape)
+	collider.radius = currentRadius
 	pass # Replace with function body.
 
 func erweitereRadius():
-	radius.shape += 10
-	print(radius)
+	currentRadius = currentRadius + radiusIncrease
+	collider.radius = currentRadius
+	print(self.name," Radius wächst auf: ",collider.radius)
 	pass
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Timer_timeout():
+	erweitereRadius()
+	pass # Replace with function body.
