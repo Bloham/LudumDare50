1bcdb3de71d4e89d2c4202cc70df1e29f3101e5d
AR <swordmageART@gmail.com>
2022-04-03 15:56:02 +0200

    added a huge clock in the sky

---
+++ SkyClock.gd
@@ -0,0 +1,16 @@
+extends Spatial
+
+
+var rotation_speed_L = .01
+var pointer_L
+var pointer_S
+
+
+func _ready():
+	pointer_L = get_node("Mesh_SkyClock_PointerL")
+	pointer_S = get_node("Mesh_SkyClock_PointerS")
+var rotation_speed_S = 60 * rotation_speed_L
+
+func _process(delta):
+	pointer_L.rotate(Vector3(0,1,0),delta * rotation_speed_L)
+	pointer_S.rotate(Vector3(0,1,0),delta * rotation_speed_S)
